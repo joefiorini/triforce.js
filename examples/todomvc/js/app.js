92c041774ee4a90eb0084f4f19fa82a5c17f424c
Joe Fiorini <joe@joefiorini.com>
2013-03-24 00:01:11 -0400

    Setup basic todomvc template

--- examples/todomvc/js/app.js
+++ examples/todomvc/js/app.js
@@ -1,6 +1,10 @@
 (function( window ) {
 	'use strict';
 
+  define('todomvc', ['triforce', 'require'], function($3, require){
+
+    return {};
+  });
 	// Your starting point. Enjoy the ride!
 
 })( window );
